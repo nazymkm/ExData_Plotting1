9e0902d1a9183f4e0f845aa178304aea113b3d77
Nazym Madaliyeva <nazym06@gmail.com>
2016-04-30 22:37:37 +1000

    legend

--- plot3.R
+++ plot3.R
@@ -15,6 +15,6 @@ lines(data$Time,data$Sub_metering_2, col="red")
 lines(data$Time,data$Sub_metering_3, col="blue")
 
 ##Adding a legend
-legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright",lty=1,col=c("black","red","blue"),cex=0.8,legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.copy(png, file="plot3.png", bg="white",width=480, height=480)
 dev.off()
